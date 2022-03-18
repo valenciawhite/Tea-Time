81713d2bc72b981d1bc99e37cb894bc36fa6e7ee
Valencia White <valenciawhite@rocketmail.com>
2022-03-18 01:56:48 -0400

    base update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import {useState, useEffect} from 'react'; 
 
 function App() {
+  const headerName = "Tea Time";
+  const [subHeader, setSubHeader] = useState("Discover local tea shops near you.");
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(function(){
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response => response.json())
+    .then(json => setPhotos(json));
+  }, []);
+
+  function SearchBttn(){
+    setSubHeader("Now let's get this par-tea started!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Header name={headerName} /> 
+      <p>{subHeader}</p>
+      <input placeholder="Search Location"></input>
+      <button className='button' onClick={SearchBttn}>Find Tea Near Me</button>
+      <ul>
+        {photos.map((photo) => 
+        <img 
+        key ={photo.id} 
+        src={photo.thumbnailUrl}
+        storename={photo.title}
+        alt={photo.title}
+        />)}
+      </ul>
       </header>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(props){
+    return (
+        <>
+         <h1>{props.name}</h1>
+        </>
+    );
+}
+
+export default Header; 
